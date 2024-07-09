fe36183097f51ea7e1b0bd5866ed6eacea3f9258
Tony_Wu <tony.wuhongtao@gmail.com>
2024-07-09 16:05:44 +0800

    add framer-motion

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,16 @@
+'use client'
 import React from 'react'
 import Link from 'next/link'
+import { motion } from "framer-motion";
 
 const HomePage = () => {
   return (
     <>
       <div className="px-4 py-8 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-10">
-        <div className="flex flex-col w-full mb-2 lg:justify-between lg:flex-row md:mb-4">
+        <div 
+          className="flex flex-col w-full mb-2 lg:justify-between lg:flex-row md:mb-4"
+          
+        >
           <div className="flex items-center group lg:max-w-xl">
             <a href="/" aria-label="Item" className="mr-3">
               <div className="flex items-center justify-center w-16 h-16 rounded-full bg-indigo-50">
@@ -29,13 +34,7 @@ const HomePage = () => {
               <div className="h-1 ml-auto duration-300 origin-left transform bg-deep-purple-accent-400 scale-x-30 group-hover:scale-x-100" />
             </h2>
           </div>
-
-          {/* <p className="w-full text-gray-700 lg:text-sm lg:max-w-md">
-            "Sed ut perspiciatis unde omnis iste natus error sit iste voluptatem
-            accusantium doloremque rem aperiam, ipsa eaque quae. Sed ut
-            perspiciatis unde omnis iste."
-          </p> */}
-          
+         
         </div>
       </div>
 
@@ -43,9 +42,12 @@ const HomePage = () => {
         <Link href="/pages/vectormix" className='flex-1 w-full md:w-1/2 lg:w-1/3'>
           <div className="carousel carousel-center rounded-box m-6 hover:shadow-lg">
             <div className="carousel-item w-full flex justify-center items-center">
-              <div className="card-body absolute text-center">
+              <motion.div className="card-body absolute text-center"
+                    initial={{ y: -120, scale: 3, rotate: 45, opacity: 0 }}
+                    animate={{ y: 0, scale:2, rotate: 0, opacity: 1 }}
+                    transition={{ ease: "linear",duration: 1}}>
                 <h2 className="card-title text-white tracking-wider">小学重难点课程推荐</h2>
-              </div>
+              </motion.div>
               <img src="../ai-teacher.jpg" alt="Pizza" />
             </div> 
           </div>
@@ -54,9 +56,12 @@ const HomePage = () => {
         <Link href="/pages/txt2img" className='flex-1 w-full md:w-1/2 lg:w-1/3'>
           <div className="carousel carousel-center rounded-box m-6 hover:shadow-lg">
             <div className="carousel-item w-full flex justify-center items-center">
-              <div className="card-body absolute text-center">
+              <motion.div className="card-body absolute text-center"
+                    initial={{ y: -120, scale: 3, rotate: -45, opacity: 0 }}
+                    animate={{ y: 0, scale:2, rotate: 0, opacity: 1 }}
+                    transition={{ ease: "linear",duration: 1}}>
                 <h2 className="card-title text-white tracking-wider">AI绘画</h2>
-              </div>
+              </motion.div>
               <img src="../ai-painting.jpg" alt="Pizza" />
             </div> 
           </div>
